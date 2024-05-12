3b0097df695bdb5b5aa47796f82f247e434d2d3a
Silviu Matei <tzevy.cont@gmail.com>
2024-05-12 09:38:54 +0300

    initial project creation

---
+++ src/main/java/com/core/velocity/spirent/birdapi/BirdapiApplication.java
@@ -0,0 +1,13 @@
+package com.core.velocity.spirent.birdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/velocity/spirent/birdapi/BirdapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.velocity.spirent.birdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
